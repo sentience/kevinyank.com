50cea1fc4e4ea292bf7076e4a9730a427de84dba
Kevin Yank <thatguy@kevinyank.com>
2022-06-13 17:56:27 +1000

    Refactor stange-new-episodes
    Make better use of async/await to maximise and manage concurrency.

--- netlify/functions/strange-new-episodes.js
+++ netlify/functions/strange-new-episodes.js
@@ -11,14 +11,14 @@ module.exports.handler = schedule("* 1-7 * * THU", handler);
 async function handler(event, context) {
   console.log("Checking for new episodes.");
 
-  const currentEpisodes = await getCurrentEpisodes();
-  const knownEpisodes = await getKnownEpisodes();
-  const newEpisodes = getNewEpisodes(currentEpisodes, knownEpisodes);
+  const current = getCurrentEpisodes();
+  const known = getKnownEpisodes();
+  const newEpisodes = getNewEpisodes(await current, await known);
   console.debug("New episodes:", JSON.stringify(newEpisodes));
 
   if (newEpisodes.length > 0) {
     await notifyNewEpisodes(newEpisodes);
-    await setKnownEpisodes(currentEpisodes);
+    await setKnownEpisodes(await current);
   }
 
   return {
@@ -26,45 +26,9 @@ async function handler(event, context) {
   };
 }
 
-async function notifyNewEpisodes(newEpisodes) {
-  if (process.env.SNE_PUSHOVER_USER && process.env.SNE_PUSHOVER_TOKEN) {
-    const p = new Push({
-      user: process.env.SNE_PUSHOVER_USER,
-      token: process.env.SNE_PUSHOVER_TOKEN,
-    });
-
-    newEpisodes.forEach(async ({ episode, title, url, airdate }) => {
-      const episodeNumber = `S1E${episode.padStart(2, "0")}`;
-      await p.send({
-        title: "Star Trek: Strange New Worlds",
-        message: `${episodeNumber}: “${title}” is now available`,
-        url: url,
-        url_title: "Watch on Paramount+",
-      });
-      console.debug("Sent Pushover notification");
-    });
-  }
-
-  if (process.env.SNE_SLACK_WEBHOOK_URL) {
-    const webhook = new IncomingWebhook(process.env.SNE_SLACK_WEBHOOK_URL);
-
-    newEpisodes.forEach(async ({ episode, title, url, airdate }) => {
-      const episodeNumber = `S1E${episode.padStart(2, "0")}`;
-      await webhook.send({
-        text:
-          `:enterprise: *Star Trek: Strange New Worlds*\n` +
-          `${episodeNumber}: _${title}_ is now available\n` +
-          `<${url}|Watch on Paramount+>`,
-        unfurl_links: true,
-      });
-      console.debug("Sent Slack notification");
-    });
-  }
-}
-
 async function getCurrentEpisodes() {
-  const response = await fetch(STRANGE_NEW_WORLDS_SEASON_1_URL);
-  const episodes = (await response.json()).result.data.map(
+  const json = await (await fetch(STRANGE_NEW_WORLDS_SEASON_1_URL)).json();
+  const episodes = json.result.data.map(
     ({ episode_number, label, url, airdate }) => ({
       episode: episode_number,
       title: label,
@@ -82,16 +46,86 @@ async function getKnownEpisodes() {
   return episodes;
 }
 
-async function setKnownEpisodes(episodes) {
-  await redis.set(SNE_REDIS_KEY, JSON.stringify(episodes));
-  console.debug("Updated known episodes:", JSON.stringify(episodes));
-}
-
 function getNewEpisodes(current, known) {
   return current.filter(
     (currentEp) =>
       !known.find((knownEp) => currentEp.episode === knownEp.episode)
   );
+}
+
+async function notifyNewEpisodes(newEpisodes) {
+  const notifiers = getNotifiers();
+  return await Promise.allSettled(
+    newEpisodes.flatMap((newEpisode) =>
+      notifiers.map((notifier) => notifier(newEpisode))
+    )
+  );
+}
+
+function getNotifiers() {
+  return [getPushoverNotifier(), getSlackNotifier()].filter((item) => !!item);
+}
+
+function getPushoverNotifier() {
+  if (process.env.SNE_PUSHOVER_USER && process.env.SNE_PUSHOVER_TOKEN) {
+    const p = new Push({
+      user: process.env.SNE_PUSHOVER_USER,
+      token: process.env.SNE_PUSHOVER_TOKEN,
+    });
+
+    return ({ episode, title, url, airdate }) => {
+      const episodeNumber = `S1E${episode.padStart(2, "0")}`;
+      return new Promise(function (resolve, reject) {
+        p.send(
+          {
+            title: "Star Trek: Strange New Worlds",
+            message: `${episodeNumber}: “${title}” is now available`,
+            url: url,
+            url_title: "Watch on Paramount+",
+          },
+          (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+          }
+        );
+      })
+        .then(() => {
+          console.debug("Sent Pushover notification");
+        })
+        .catch((err) => {
+          console.error("Error sending Pushover notification", err);
+        });
+    };
+  }
+}
+
+function getSlackNotifier() {
+  if (process.env.SNE_SLACK_WEBHOOK_URL) {
+    const webhook = new IncomingWebhook(process.env.SNE_SLACK_WEBHOOK_URL);
+
+    return ({ episode, title, url, airdate }) => {
+      const episodeNumber = `S1E${episode.padStart(2, "0")}`;
+      return webhook
+        .send({
+          text:
+            `:enterprise: *Star Trek: Strange New Worlds*\n` +
+            `${episodeNumber}: _${title}_ is now available\n` +
+            `<${url}|Watch on Paramount+>`,
+          unfurl_links: true,
+        })
+        .then(() => {
+          console.debug("Sent Slack notification");
+        })
+        .catch((err) => {
+          console.error("Error sending Slack notification", err);
+        });
+    };
+  }
+}
+
+async function setKnownEpisodes(episodes) {
+  await redis.set(SNE_REDIS_KEY, JSON.stringify(episodes));
+  console.debug("Updated known episodes:", JSON.stringify(episodes));
 }
 
 const STRANGE_NEW_WORLDS_SEASON_1_URL =
