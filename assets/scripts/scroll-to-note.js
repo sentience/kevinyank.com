8846b46154579a46f604f87f77b7f4a112c654f2
Kevin Yank <thatguy@kevinyank.com>
2023-01-08 01:05:53 +1100

    Implement scroll-to-next/previous note buttons

--- assets/scripts/scroll-to-note.js
+++ assets/scripts/scroll-to-note.js
@@ -1,31 +1,61 @@
 "use strict";
 
+defer(() => {
+  new ScrollToNotes(document.getElementById("recent-notes"));
+});
+
 class ScrollToNotes {
   constructor(domNode) {
     this.domNode = domNode;
-    this.scrollButtons = Array.from(
-      domNode.querySelectorAll(".scroll-to-note")
-    ).map((node) => new ScrollButton(node));
-  }
-}
+    this.notes = domNode.querySelectorAll(".scroll-to-note");
 
-class ScrollButton {
-  constructor(domNode) {
-    this.domNode = domNode;
-    this.target = document.getElementById(
-      `note-${domNode.getAttribute("id").match(/scroll-to-note-(\w+)/)[1]}`
-    );
+    (this.scrollToNextButton = domNode.querySelector(
+      ".scroll-to-note-next"
+    )).addEventListener("click", () => this.scrollToNext());
 
-    domNode.addEventListener("click", this.onClick.bind(this));
+    (this.scrollToPreviousButton = domNode.querySelector(
+      ".scroll-to-note-previous"
+    )).addEventListener("click", () => this.scrollToPrevious());
+
+    this.updateButtonStates();
   }
 
-  onClick() {
-    this.target.scrollIntoView({
+  scrollToNext() {
+    const index = this.getCurrentScrollIndex() + 1;
+    if (this.notes[index]) this.scrollToIndex(index);
+  }
+
+  scrollToPrevious() {
+    const index = this.getCurrentScrollIndex() - 1;
+    if (this.notes[index]) this.scrollToIndex(index);
+  }
+
+  scrollToIndex(index) {
+    this.notes[index].scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "start",
     });
+    this.updateButtonStates(index);
+  }
+
+  updateButtonStates(index = this.getCurrentScrollIndex()) {
+    this.scrollToNextButton.disabled = index + 1 >= this.notes.length;
+    this.scrollToPreviousButton.disabled = index <= 0;
+  }
+
+  getCurrentScrollIndex() {
+    // NOTE: this doesn't yet support RTL, where scrollX is negative as
+    // scrolling occurs, and we should consult offsetRight, not offsetLeft.
+    const scrollPadding = parseInt(
+      getComputedStyle(this.notes[0].parentNode).paddingInlineStart
+    );
+    return Array.from(this.notes).findIndex(
+      (note) => note.offsetLeft >= note.parentNode.scrollLeft - scrollPadding
+    );
   }
 }
 
-new ScrollToNotes(document.getElementById("recent-notes"));
+function defer(callbackFn) {
+  setTimeout(callbackFn, 0);
+}
