c83893030a72199591ab1c10b21d9393c64a6bcc
Kevin Yank <thatguy@kevinyank.com>
2023-01-21 18:16:00 +1100

    Upgrade to Eleventy 2.0.0-beta.1

--- .eleventy.js
+++ .eleventy.js
@@ -92,7 +92,6 @@ function setUpMarkdown(eleventyConfig) {
     html: true,
     typographer: true,
   });
-  markdownIt.disable("code"); // Don't trigger code blocks with indented HTML in Liquid partials (will be default in Eleventy 2.0): https://www.11ty.dev/docs/languages/markdown/#indented-code-blocks
   markdownIt.use(anchor, {
     permalink: anchor.permalink.linkAfterHeader({
       class: "heading-anchor",
