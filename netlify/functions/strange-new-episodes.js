4b14e5363af939bce1111abe5a96bca70b3a035a
Kevin Yank <thatguy@kevinyank.com>
2023-02-16 18:54:09 +1100

    Use User-Agent that Paramount+ likes
    Paramount+ was only sending promotional clips to requests from the default node-fetch user agent.

--- netlify/functions/strange-new-episodes.js
+++ netlify/functions/strange-new-episodes.js
@@ -30,7 +30,14 @@ async function processShow(show) {
 }
 
 async function getCurrentEpisodes(show) {
-  const json = await (await fetch(show.season_json)).json();
+  const json = await (
+    await fetch(show.season_json, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15",
+      },
+    })
+  ).json();
   const episodes = json.result.data
     .filter(({ type }) => type === "Full Episode")
     .map(({ episode_number, label, url, airdate }) => ({
