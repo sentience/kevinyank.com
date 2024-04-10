8271e587f7bb7315290a6c8d3db9bc011b3b91a5
Kevin Yank <thatguy@kevinyank.com>
2024-04-11 08:52:08 +1000

    Add DIS S5, SNW S3, LD S5

--- netlify/functions/strange-new-episodes.js
+++ netlify/functions/strange-new-episodes.js
@@ -174,20 +174,29 @@ async function setKnownEpisodes(show, episodes) {
 
 const SHOWS = [
   {
+    abbr: "DIS",
+    name: "Star Trek: Discovery",
+    current_season: 5,
+    season_json:
+      "https://www.paramountplus.com/shows/star-trek-discovery/xhr/episodes/page/0/size/18/xs/0/season/5/",
+    redis_key: "discovery-episodes",
+    notifications: true,
+  },
+  {
     abbr: "SNW",
     name: "Star Trek: Strange New Worlds",
-    current_season: 2,
+    current_season: 3,
     season_json:
-      "https://www.paramountplus.com/shows/star-trek-strange-new-worlds/xhr/episodes/page/0/size/18/xs/0/season/2/",
+      "https://www.paramountplus.com/shows/star-trek-strange-new-worlds/xhr/episodes/page/0/size/18/xs/0/season/3/",
     redis_key: "strange-new-episodes",
     notifications: true,
   },
   {
     abbr: "LD",
     name: "Star Trek: Lower Decks",
-    current_season: 4,
+    current_season: 5,
     season_json:
-      "https://www.paramountplus.com/shows/star-trek-lower-decks/xhr/episodes/page/0/size/18/xs/0/season/4/",
+      "https://www.paramountplus.com/shows/star-trek-lower-decks/xhr/episodes/page/0/size/18/xs/0/season/5/",
     redis_key: "lower-decks-episodes",
     notifications: true,
   },
